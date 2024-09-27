f4173db134b328f270dbc395df80fe673d9b3136
liver-juice <113276155+liver-juice@users.noreply.github.com>
2024-09-26 19:11:17 -0500

    restructured methods to use single responsibility
    better principals implemented.
    fetches api async and displays data

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let weatherApp = (function() {
     let city = 'beaumont tx';
     let apiKey = 'DLNE4QADMLDWQ9VRNE3ZP6U4R';
+    let weatherWeek;
 
-    // the variables that will hold references to changing weather data
-    let weatherWeek = [];
-    let today = weatherWeek[0];
 
     let getWeatherData = async () => {
+        // calls api, returns a week of weather data
         let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`;
 
         try {
@@ -17,7 +16,8 @@ let weatherApp = (function() {
             // extract what we need from it
             let days = responseJson.days;
             weatherWeek = [days[0], days[1], days[2], days[3], days[4], days[5], days[6]];
-            today = weatherWeek[0];
+            return weatherWeek;
+
 
         } catch (error) {
             console.log(`erra! :${error}`);
@@ -25,21 +25,87 @@ let weatherApp = (function() {
     }
 
     let updateCity = () => {
+        // updates the city value. thats it
         let cityInput = document.querySelector('.cityinput').value;
-        if(cityInput !== ''){
-            city = cityInput;
-            console.log(city)
-        }else{
-            console.log('enter a city first');
-        }
-        getWeatherData();
-        console.log(today);
+        city = cityInput;
     }
 
+    let drawToday = () => {
+        let today = weatherWeek[0];
+        console.log(today);
+        
+        // reference todays div elements
+
+        let citydiv = document.querySelector('.citydiv');
+        let cityindicator = document.querySelector('.cityindicator');
+        let datediv = document.querySelector('.datediv');
+        let imgdiv = document.querySelector('.imgdiv');
+        let tempdiv = document.querySelector('.tempdiv');
+        let statediv = document.querySelector('.statediv');
+
+        // fill it in mf
+        citydiv.textContent = city;
+        cityindicator.textContent = city;
+        datediv.textContent = today.datetime;
+        imgdiv.textContent = today.icon;
+        tempdiv.textContent = today.temp;
+        statediv.textContent = today.description;
+
+    }
+
+    let drawWeek = () => {
+        weatherWeek.forEach(day => {
+            // for each day, create an appropriate daydiv
+            let daydiv = document.createElement('div');
+            daydiv.classList.add('daydiv');
+
+            let icondiv = document.createElement('div');
+            icondiv.classList.add('weekicon');
+            let datediv = document.createElement('div');
+            datediv.classList.add('weekdate');
+            let lowdiv = document.createElement('div');
+            lowdiv.classList.add('weeklow');
+            let highdiv = document.createElement('div');
+            highdiv.classList.add('weekhigh');
+
+            daydiv.appendChild(icondiv);
+            daydiv.appendChild(datediv);
+            daydiv.appendChild(lowdiv);
+            daydiv.appendChild(highdiv);
+            
+            // slot the day objects data into the daydiv
+            icondiv.textContent = 'ICON';
+            datediv.textContent = 'DATE';
+            lowdiv.textContent = 'Low: ';
+            highdiv.textContent = 'High: ';
+
+            // append the daydiv inside of the weekdiv onscreen.
+            let weekdiv = document.querySelector('.weekdiv');
+            weekdiv.appendChild(daydiv);
+        });
+    }
+    
+    let drawData = () => {
+        drawToday();
+        drawWeek();
+    }
+
+
     let refreshBtn = document.querySelector('.refreshbtn');
-    refreshBtn.addEventListener('click', updateCity);
+    refreshBtn.addEventListener('click', async () => {
+        let cityInput = document.querySelector('.cityinput').value;
+        if(cityInput !== ''){
+            updateCity();
+        }
+        await getWeatherData();
+        drawData();
+    });
+
+    (async () => {
+    await getWeatherData();
+    drawData();
+    })();
 
 
-    getWeatherData()
 
 })();
